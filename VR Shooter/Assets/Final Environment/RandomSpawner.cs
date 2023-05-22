61eb899512408cb29e3b8e05e43b55ae65718b7f
zhaona99 <guozhaona99@gmail.com>
2023-05-22 14:34:02 +0200

    final environment continous

--- VR Shooter/Assets/Final Environment/RandomSpawner.cs
+++ VR Shooter/Assets/Final Environment/RandomSpawner.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class RandomSpawner : MonoBehaviour
 {
     public GameObject[] myObjects;
+    public GameObject[] myContainers;
+    public GameObject npc;
+    public int counter1, counter2, countContainer;
    
     void Update(){
-        //for(int i = 0; i < 50, i++){
+        if(counter1 < 250){
             int rand = Random.Range(0, myObjects.Length);
             Vector3 randomSpawnPosition = new Vector3(Random.Range(-10,11),5,Random.Range(-10,11));
             Instantiate(myObjects[rand],randomSpawnPosition,Quaternion.identity);
-       // }
-
+            counter1++;
+        }
+        if(countContainer < 25){
+            int rand = Random.Range(0, myContainers.Length);
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-10,11),5,Random.Range(-10,11));
+            Instantiate(myContainers[rand],randomSpawnPosition,Quaternion.identity);
+            countContainer++;
+        }
+        if(counter1 >= 250 && counter2 < 100){
+            int rand = Random.Range(0, myObjects.Length);
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-10,11),5,Random.Range(-10,11));
+            Instantiate(npc,randomSpawnPosition,Quaternion.identity);
+            counter2++;
+        }
     }
 }
