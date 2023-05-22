887b58966299c08ac0acf49408197740347dab46
zhaona99 <guozhaona99@gmail.com>
2023-05-22 13:08:03 +0200

    general creation of final environment

---
+++ VR Shooter/Assets/Final Environment/RandomSpawner.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public GameObject[] myObjects;
+   
+    void Update(){
+        //for(int i = 0; i < 50, i++){
+            int rand = Random.Range(0, myObjects.Length);
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-10,11),5,Random.Range(-10,11));
+            Instantiate(myObjects[rand],randomSpawnPosition,Quaternion.identity);
+       // }
+
+    }
+}
