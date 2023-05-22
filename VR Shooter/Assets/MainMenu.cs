aed4a284c59dd8c2f26b8094137420cee15f65b8
emma-xibao <74651477+emma-xibao@users.noreply.github.com>
2023-05-22 14:41:50 +0200

    small modified

--- VR Shooter/Assets/MainMenu.cs
+++ VR Shooter/Assets/MainMenu.cs
@@ -5,8 +5,5 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame(){
-        SceneManagement.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
-
-    }
+    
 }
