1477b8898f29cb35f8ab1acc178f401e0ba48374
zhaona99 <guozhaona99@gmail.com>
2023-05-23 13:55:14 +0200

    fallingObjects

---
+++ VR Shooter/Assets/Final Environment/Falling Objects/FallingObjects.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForFalling());
+    }
+
+    IEnumerator WaitForFalling()
+    {
+        yield return new WaitForSeconds(10);
+        GameObject[] fallingObjects = GameObject.FindGameObjectsWithTag("fallingObjects");
+        
+        foreach (GameObject falling in fallingObjects)
+        {
+            Rigidbody rb = falling.GetComponent<Rigidbody>();
+            rb.isKinematic = true;
+            rb.detectCollisions = false;
+        }
+
+    }
+}
